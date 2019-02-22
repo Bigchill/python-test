626d531a802b59a2e74ec52eeed9e8c06db7f9d7
Bigchill <47869957+Bigchill@users.noreply.github.com>
2019-02-22 19:14:26 +0530

    Update part-1.py

--- part-1.py
+++ part-1.py
@@ -10,6 +10,8 @@ sbin = get_history(symbol='SBIN',start=date(2018,1,1),end=date(2019,2,22))  #sam
 print(sbin)
 print(sbin["Close"])
 
+
+
 def moving_average(dataset, moving_limit, attribute):
   """
   calculates moving averages for an attribute(closing price) for each stock and index. moving limit is set for how many weeks do we want to
@@ -17,11 +19,29 @@ def moving_average(dataset, moving_limit, attribute):
   """
     mean=[]
     for i in range(4, moving_limit+1, 12):
-        avg = dataset[attribute].iloc[:i].mean()
+        avg = dataset[attribute].iloc[:i].mean()        #Could use Dynamic Programming here for optimization.
         mean.append(avg)
     return mean
 
-mean = moving_average(sbin, 52, "Close")
+mean = moving_average(dataset = sbin,moving_limit = 52, attribute = "Close")
 print(mean)
+
+
+
+def rolling_window(dataset, size, limit):
+"""
+displays data for all n consecutive entries in the dataset(where n = size), up untill the size(window size) is increased to a limit  
+"""
+    while size <= limit:
+        end = size
+        i = 0
+        while end <= len(dataset.index):
+            print(dataset.iloc[i:end])
+            end+=1
+            i+=1
+        size+=1
+
+
+rolling_window(sbin, 10, 75)
 
 
